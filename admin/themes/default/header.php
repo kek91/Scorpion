a638f914c99d207152cd9618adce98c995d8cf01
Teknix <kek@teknix.no>
2016-06-10 11:40:29 +0200

    Move metatag higher.

--- admin/themes/default/header.php
+++ admin/themes/default/header.php
@@ -3,10 +3,10 @@
     <head>
         <title>ScorpionCMS Admin</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="themes/default/bootstrap.yeti.min.css" type="text/css">
         <link rel="stylesheet" href="themes/default/style.css" type="text/css">
         <script src="themes/default/default.js"></script>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
 
