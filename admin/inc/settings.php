1dea348f501399f6fba8b46d06b951e341a2fb36
kek <kek@teknix.no>
2016-06-12 20:18:34 +0200

    Settings page wip + minor css add

--- admin/inc/settings.php
+++ admin/inc/settings.php
@@ -1,2 +1,32 @@
 <h1>Settings</h1>
 
+<a href="settings/account"><button class="btn btn-primary">Account preferences</button></a>
+<a href="settings/users"><button class="btn btn-default">User administration</button></a>
+<a href="settings/system"><button class="btn btn-default">System configuration</button></a>
+
+<?php
+if(isset($subpage)) {
+    if($subpage == "account") {
+        include("subsettings/account.php");
+    }
+    elseif($subpage == "users") {
+        include("subsettings/users.php");
+    }
+    elseif($subpage == "system") {
+        include("subsettings/system.php");
+    }
+}
+else {
+    include("subsettings/account.php");
+}
+
+
+
+
+
+
+
+
+
+
+
