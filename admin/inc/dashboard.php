3bb6947576061b9e9513427029bc41cf36787c6f
kek <kek@teknix.no>
2016-06-12 20:18:12 +0200

    Various js graphs for website statistics etc

--- admin/inc/dashboard.php
+++ admin/inc/dashboard.php
@@ -1,22 +1,59 @@
 <h1>Dashboard</h1>
 
-<div class="alert alert-dismissable alert-info" role="alert">
-<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-<div id="funny-quote"></div>
-<!--<br><br><button type="button" class="btn btn-sm btn-danger">Disable funny quotes?</button>-->
+<div class="alert alert-danger">
+    <b>Note - Scorpion CMS Beta</b><br><br>
+    Statistics data on "Dashboard" are for demonstration purposes and not actually real data.
 </div>
 
+<!--<div class="panel panel-primary">...</div>
+<div class="panel panel-success">...</div>
+<div class="panel panel-info">...</div>
+<div class="panel panel-warning">...</div>
+<div class="panel panel-danger">...</div>-->
+<div class="container col-md-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Unique visitors &nbsp; 
+            <button onClick="show_chart_visitors_monthly();" id="visitors-monthly" class="btn btn-sm btn-primary">Monthly</button>
+            <button onClick="show_chart_visitors_weekly();" id="visitors-weekly" class="btn btn-sm btn-default">Weekly</button>
+            <button type="button" class="close close-canvas" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="panel-body">
+            <canvas id="chart-visitors" id="chart-visitors-parent" height="100"></canvas>
+        </div>
+    </div>
+</div>
+<div class="container col-md-6">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Popular pages
+            <button type="button" class="close close-canvas" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="panel-body">
+            <canvas id="chart-popular"></canvas>
+        </div>
+    </div>
+</div>
+<div class="container col-md-6">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Referrals
+            <button type="button" class="close close-canvas" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="panel-body">
+            <div id="chart-referrals"></div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js"></script>
+
 <script>
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function (data) {
-        if(xhr.readyState === 4) {
-            if(xhr.status === 200) {
-                var obj = JSON.parse(xhr.responseText);
-                console.log(obj['value']['joke']);
-                document.getElementById('funny-quote').innerHTML = obj['value']['joke'];
-            }
-        }
-    }
-    xhr.open('GET', 'http://api.icndb.com/jokes/random?limitTo=[nerdy]');
-    xhr.send();
+show_chart_visitors_monthly();
+
+show_chart_popular();
+
+show_chart_referrals();
+
+
 </script>
--- admin/themes/default/default.js
+++ admin/themes/default/default.js
@@ -3,9 +3,134 @@ document.addEventListener('DOMContentLoaded', function() {
     /* Support for closing alert boxes */
     [].forEach.call(document.querySelectorAll('.close'), function(e) {
         e.addEventListener('click', function() {
-            this.parentNode.className += ' hidden';
+            if(this.parentNode.className == 'panel-heading') {
+                this.parentNode.parentNode.className += ' hidden';
+            }
+            else {
+                this.parentNode.className += ' hidden';
+            }
         })
     });
 
 
 });
+
+
+
+function get_funny_quote() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function (data) {
+        if(xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                var obj = JSON.parse(xhr.responseText);
+                console.log(obj['value']['joke']);
+                document.getElementById('funny-quote').innerHTML = obj['value']['joke'];
+            }
+        }
+    }
+    xhr.open('GET', 'http://api.icndb.com/jokes/random?limitTo=[nerdy]');
+    xhr.send();
+}
+
+
+
+function show_chart_visitors_monthly() {
+    document.getElementById('visitors-monthly').className = 'btn btn-sm btn-primary';
+    document.getElementById('visitors-weekly').className += 'btn btn-sm btn-default';
+    var ctx = document.getElementById("chart-visitors");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                label: 'Unique visitors',
+                data: [12, 19, 3, 5, 11, 8],
+                fill: false,
+                lineTension: 0,
+                borderColor: "rgba(255, 153, 102, 1)",
+                borderCapStyle: 'butt',
+                pointBorderColor: "rgba(255, 153, 102, 1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 8,
+                pointHoverRadius: 15,
+                pointHoverBackgroundColor: "rgba(255, 153, 102, 0.8)",
+                pointHoverBorderColor: "rgba(255, 153, 102, 1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 2,
+                pointHitRadius: 5
+            }]
+        },
+        options: {
+            legend: {
+                display:false
+            },
+        }
+    });
+}
+
+function show_chart_visitors_weekly() {
+    document.getElementById('visitors-monthly').className = 'btn btn-sm btn-default';
+    document.getElementById('visitors-weekly').className += 'btn btn-sm btn-primary';
+    var ctx = document.getElementById("chart-visitors");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Monday", "Thirsday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+            datasets: [{
+                label: 'Unique visitors',
+                data: [1, 2, 5, 2, 3, 7, 5],
+                fill: false,
+                lineTension: 0,
+                borderColor: "rgba(255, 153, 102, 1)",
+                borderCapStyle: 'butt',
+                pointBorderColor: "rgba(255, 153, 102, 1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 8,
+                pointHoverRadius: 15,
+                pointHoverBackgroundColor: "rgba(255, 153, 102, 0.8)",
+                pointHoverBorderColor: "rgba(255, 153, 102, 1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 2,
+                pointHitRadius: 5
+            }]
+        },
+        options: {
+            legend: {
+                display:false
+            },
+        }
+    });
+}
+
+function show_chart_popular() {
+    var ctx = document.getElementById("chart-popular");
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["/posts", "/post/hello-world", "/post/guide-to-programming", "/posts/world-of-warcraft", "/posts/hardware"],
+            datasets: [{
+                label: 'Unique visitors',
+                data: [12, 19, 3, 5, 2],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)',
+                    'rgba(75, 192, 192, 0.8)',
+                    'rgba(153, 102, 255, 0.8)'
+                ],
+            }]
+        }
+    });
+}
+
+function show_chart_referrals() {
+    var ctx = document.getElementById("chart-referrals");
+    ctx.innerHTML = '<div class="list-group">' +
+            '<a href="https://example.com/123/post456.html" target="_blank" class="list-group-item"><span class="badge">14</span>https://example.com/123/post456.html</a>' +
+            '<a href="https://hello.org/lorem-ipsum-dolor-sit-amet" target="_blank" class="list-group-item"><span class="badge">6</span>https://hello.org/lorem-ipsum-dolor-sit-amet</a>' + 
+            '<a href="https://world.info/foo/bar" target="_blank" class="list-group-item"><span class="badge">2</span>https://world.info/foo/bar</a>' + 
+            '<a href="https://github.com/kek91/Scorpion" target="_blank" class="list-group-item"><span class="badge">1</span>https://github.com/kek91/Scorpion</a>' + 
+            '<a href="https://github.com/kek91/Scorpion/tree/admin" target="_blank" class="list-group-item"><span class="badge">1</span>https://github.com/kek91/Scorpion/tree/admin</a>';
+            '</div>';
+
+}
