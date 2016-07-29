1fb1eec2eaeb0925193129c729eb035982b03386
Teknix <kek@teknix.no>
2016-07-29 10:46:50 +0200

    Added hover effect on navigation links

--- admin/inc/modules/nav.php
+++ admin/inc/modules/nav.php
@@ -1,18 +1,18 @@
                         <?php
                         if($user->isLoggedIn()) {
                         ?>    
-                        <li><a href="dashboard">dashboard</a></li>
-                        <li><a href="posts">posts</a></li>
-                        <li><a href="pages">pages</a></li>
-                        <li><a href="media">media</a></li>
-                        <li><a href="settings">settings</a></li>
-                        <li><a href="logout">logout</a></li>
+                        <li><a class="hvr-underline-from-center" href="dashboard">dashboard</a></li>
+                        <li><a class="hvr-underline-from-center" href="posts">posts</a></li>
+                        <li><a class="hvr-underline-from-center" href="pages">pages</a></li>
+                        <li><a class="hvr-underline-from-center" href="media">media</a></li>
+                        <li><a class="hvr-underline-from-center" href="settings">settings</a></li>
+                        <li><a class="hvr-underline-from-center" href="logout">logout</a></li>
                         <?php
                         }
                         else {
                         ?>
-                        <li><a href="login">login</a></li>
-                        <li><a href="../index">go back to website</a></li>
+                        <li><a class="hvr-underline-from-center" href="login">login</a></li>
+                        <li><a class="hvr-underline-from-center" href="../index">go back to website</a></li>
                         <?php
                         }
                         ?>
